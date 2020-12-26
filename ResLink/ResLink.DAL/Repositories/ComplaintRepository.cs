622deb1e0935795a230acb45b542c75fdc571029
Mohau <mohaum12@gmail.com>
2020-12-27 00:00:25 +0200

    Add- Added The Respositories Complaint and Event to the ResLink.DAL

---
+++ ResLink/ResLink.DAL/Repositories/ComplaintRepository.cs
@@ -0,0 +1,48 @@
+﻿using ResLink.BL.Models;
+using ResLink.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResLink.DAL.Repositories
+{
+    public class ComplaintRepository
+    {
+        private ResLinkDatabase db = null;
+        protected static ComplaintRepository instance;
+
+        static ComplaintRepository()
+        {
+            instance = new ComplaintRepository();
+        }
+
+        protected ComplaintRepository()
+        {
+            db = new ResLinkDatabase();
+        }
+
+
+        public static IEnumerable<Complaint> GetComplaint()
+        {
+            return instance.db.GetItems<Complaint>();
+        }
+
+        public static Complaint GetComplaintById(string id)
+        {
+            return instance.db.GetItem<Complaint>(id);
+        }
+
+        public static void SaveComplaint(Complaint item)
+        {
+            instance.db.SaveItem<Complaint>(item);
+        }
+
+        public static void DeleteComplaint(string objectId)
+        {
+            instance.db.DeleteItem<Complaint>(objectId);
+        }
+
+    }
+}
---
+++ ResLink/ResLink.DAL/Repositories/EventRepository.cs
@@ -0,0 +1,47 @@
+﻿using ResLink.BL.Models;
+using ResLink.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResLink.DAL.Repositories
+{
+    public class EventRepository
+    {
+        private ResLinkDatabase db = null;
+        protected static EventRepository instance;
+
+        static EventRepository()
+        {
+            instance = new EventRepository();
+        }
+
+        protected EventRepository()
+        {
+            db = new ResLinkDatabase();
+        }
+
+
+        public static IEnumerable<Event> GetEvent()
+        {
+            return instance.db.GetItems<Event>();
+        }
+
+        public static Event GetEventById(string id)
+        {
+            return instance.db.GetItem<Event>(id);
+        }
+
+        public static void SaveEvent(Event item)
+        {
+            instance.db.SaveItem<Event>(item);
+        }
+
+        public static void DeleteEvent(string objectId)
+        {
+            instance.db.DeleteItem<Event>(objectId);
+        }
+    }
+}
