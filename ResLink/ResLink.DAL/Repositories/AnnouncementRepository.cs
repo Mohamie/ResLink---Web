53cd2c7219ccacdb8a0e1c4c4443d2d86e81f348
Nelson Mohamie <218004055@stud.cut.ac.za>
2020-12-26 22:46:56 +0200

    Add - Added ResLinkDatabase class in DL, this class contains generic methods for retreival and persistance based on Backendless ORM. Also added one repository 'AnnouncementRepo'

---
+++ ResLink/ResLink.DAL/Repositories/AnnouncementRepository.cs
@@ -0,0 +1,49 @@
+﻿using ResLink.BL.Models;
+using ResLink.DL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResLink.DAL
+{
+    public class AnnouncementRepository
+    {
+        private ResLinkDatabase db = null;
+        protected static AnnouncementRepository instance;
+
+        static AnnouncementRepository()
+        {
+            instance = new AnnouncementRepository();
+        }
+
+        protected AnnouncementRepository()
+        {
+            db = new ResLinkDatabase();
+        }
+
+
+        public static IEnumerable<Announcement> GetAnnouncements()
+        {
+            return instance.db.GetItems<Announcement>();
+        }
+
+        public static Announcement GetAnnouncementById(string id)
+        {
+            return instance.db.GetItem<Announcement>(id);
+        }
+
+        public static void SaveAnnouncement(Announcement item)
+        {
+            instance.db.SaveItem<Announcement>(item);
+        }
+
+        public static void DeleteAnnouncement( string objectId)
+        {
+            instance.db.DeleteItem<Announcement>(objectId);
+        }
+
+        
+    }
+}
---
+++ ResLink/ResLink.DL/ResLinkDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BackendlessAPI;
+
+namespace ResLink.DL
+{
+    public class ResLinkDatabase
+    {
+        public IEnumerable<T> GetItems<T>()
+        {
+            return Backendless.Data.Of<T>().Find();
+        }
+
+        public T GetItem<T>(string id)
+        {
+            return Backendless.Data.Of<T>().FindById(id);
+        }
+
+        public T SaveItem<T>( T item )
+        {
+            return Backendless.Data.Of<T>().Save(item);
+        }
+
+        public void DeleteItem<T>( string objectId)
+        {
+            Backendless.Data.Of<T>().Remove($"objectId = '{objectId}'");
+        }
+    }
+}
