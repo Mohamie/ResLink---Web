13d37a64a4c269acba22439fedf610424e32a505
Nelson Mohamie <218004055@stud.cut.ac.za>
2021-01-13 03:13:34 +0200

    Refactor - ResLink.DL Database Main Access methods from sync to async

--- ResLink/ResLink.DL/ResLinkDatabase.cs
+++ ResLink/ResLink.DL/ResLinkDatabase.cs
@@ -9,25 +9,24 @@ namespace ResLink.DL
 {
     public class ResLinkDatabase
     {
-        public IEnumerable<T> GetItems<T>()
+        public async Task<IEnumerable<T>> GetItems<T>()
         {
-            return Backendless.Data.Of<T>().Find();
+            return await Backendless.Data.Of<T>().FindAsync();
         }
 
-        public T GetItem<T>(string id)
+        public async Task<T> GetItem<T>(string id)
         {
-            return Backendless.Data.Of<T>().FindById(id);
+            return await Backendless.Data.Of<T>().FindByIdAsync(id);
         }
 
-        public T SaveItem<T>( T item )
+        public async Task<T> SaveItem<T>( T item )
         {
-            return Backendless.Data.Of<T>().Save(item);
+            return await Backendless.Data.Of<T>().SaveAsync(item);
         }
 
-        public void DeleteItem<T>( string objectId)
+        public async Task DeleteItem<T>( string objectId)
         {
-            Backendless.Data.Of<T>().Remove($"objectId = '{objectId}'");
-
+            await Backendless.Data.Of<T>().RemoveAsync($"objectId = '{objectId}'");
         }
     }
 }
