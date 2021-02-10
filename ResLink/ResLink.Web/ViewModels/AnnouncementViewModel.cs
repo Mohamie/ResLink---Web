6f218512bf81790d3a5a117633020dba76f472fc
Mohau <mohaum12@gmail.com>
2021-02-10 04:41:55 +0200

    Added : Announcement, Event and Complaint ViewModels

---
+++ ResLink/ResLink.Web/ViewModels/AnnouncementViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;Announcement
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResLink.Web.ViewModels
+{
+    public class AnnouncementViewModel
+    {
+
+        [Required]
+        public string AnnouncementTitle { get; set; }
+
+        [Required]
+        public string AnnouncementDescription { get; set; }
+        [Required]
+        public string AnnouncementCategory { get; set; }
+
+        [Required]
+        public string AnnouncementAlertSeverity { get; set; }
+
+    }
+}
---
+++ ResLink/ResLink.Web/ViewModels/EventViewModel.cs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResLink.Web.ViewModels
+{
+    public class EventViewModel
+    {
+        [Required]
+        public string EventTitle { get; set; }
+
+        [Required]
+        public string EventDescription { get; set; }
+        [Required]
+        public string EventCategory { get; set; }
+
+    }
+}
